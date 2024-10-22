897ea87ea0891268d1db6b8cafba68a30db52e68
GustavoChp <gustavochp@users.noreply.github.com>
2024-10-22 00:21:17 -0300

    Proy2.2.3

--- main.py
+++ main.py
@@ -38,4 +38,31 @@ def div_calc(num1, num2):
         print(f"Es una operación de división, el resultado es: {calc:.2}")
     else: print(f"El divisor es 0 y la división por cero no se puede calcular")
 
+def main():
+    #hist_Calc = []
+    clear_console()
+    while True:
+        num01=float(input("Ingrese primer valor: "))
+        num02=float(input("Ingrese segundo valor: "))
+        display_menu()
+        choice = input("Seleccione una opción (1-4 / 0 para salir): ")
+
+        if choice == "1":
+            suma_calc(num01, num02)
+        elif choice == "2":
+            resta_calc(num01, num02)
+        elif choice == "3":
+            multip_calc(num01, num02)
+        elif choice == "4":
+            div_calc(num01, num02)
+        elif choice == "0":
+            print("Salir de la calculadora!")
+            break
+        else:
+            print("No es una opción válida. Tratar de nuevo.")
+        
+
+if __name__ == "__main__":
+    main()
+
 # _Fin de programa Test 2.0.1
