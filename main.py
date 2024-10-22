fc482f098b3c3ee42807f7d053c7f215f30f062e
GustavoChp <gustavochp@users.noreply.github.com>
2024-10-22 20:29:24 -0300

    Proy02.2.3

--- main.py
+++ main.py
@@ -12,48 +12,49 @@ def clear_console():
 
 def display_menu():
     # clear_console()
-    print("\n--- Menu Calculadora ---")
-    print("\n--- Ingresar 2 valores y luego la operación ---")
-    print("1. + Suma")
-    print("2. - Resta")
-    print("3. * Multiplicación")
-    print("4. / División")
+    print("\n--- Menu Calculadora. Indicar la Operación: ---")
+    #print("\n--- Ingresar 2 valores y luego la operación ---")
+    print("+ . Suma")
+    print("- . Resta")
+    print("* . Multiplicación")
+    print("/ . División")
     print("0. Salir")
 
 def suma_calc(num1, num2):
     calc=num1 + num2
-    print(f"Es una operación de suma, el resultado es: {calc:.2}")
+    print (f"{num1:0.1f} + {num2:0.1f} = {calc:0.1f}")
 
 def resta_calc(num1, num2):
     calc=num1 - num2
-    print(f"Es una operación de resta, el resultado es: {calc:.2}")
+    print (f"{num1:0.1f} - {num2:0.1f} = {calc:0.1f}")
     
 def multip_calc(num1, num2):
     calc=num1 * num2
-    print(f"Es una operación de multiplicación, el resultado es: {calc:.2}")
+    print (f"{num1:0.1f} * {num2:0.1f} = {calc:0.1f}")
 
 def div_calc(num1, num2):
     if num2!=0:
         calc=num1 / num2
-        print(f"Es una operación de división, el resultado es: {calc:.2}")
+        print (f"{num1:0.1f} / {num2:0.1f} = {calc:0.1f}")
     else: print(f"El divisor es 0 y la división por cero no se puede calcular")
 
 def main():
-    #hist_Calc = []
+    hist_Calc = []
     clear_console()
     while True:
+        print("\n--- Ingresar 2 valores y luego la operación ---")
         num01=float(input("Ingrese primer valor: "))
         num02=float(input("Ingrese segundo valor: "))
         display_menu()
-        choice = input("Seleccione una opción (1-4 / 0 para salir): ")
-
-        if choice == "1":
+        choice = input("Seleccione una operación (0 para salir): ")
+        print("")
+        if choice == "+":
             suma_calc(num01, num02)
-        elif choice == "2":
+        elif choice == "-":
             resta_calc(num01, num02)
-        elif choice == "3":
+        elif choice == "*":
             multip_calc(num01, num02)
-        elif choice == "4":
+        elif choice == "/":
             div_calc(num01, num02)
         elif choice == "0":
             print("Salir de la calculadora!")
@@ -61,8 +62,9 @@ def main():
         else:
             print("No es una opción válida. Tratar de nuevo.")
         
+        print("")
 
 if __name__ == "__main__":
     main()
 
-# _Fin de programa Test 2.0.1
+# _Fin de programa
